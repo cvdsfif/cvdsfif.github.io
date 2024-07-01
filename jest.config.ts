4408e82310d644428a3dd4ed191acb7184eee4ba
Nikit Zykov <nikit@zykov.com>
2024-07-01 13:37:03 +0200

    Initial commit

---
+++ jest.config.ts
@@ -0,0 +1,21 @@
+export default {
+  preset: "ts-jest",
+  testEnvironment: "jest-environment-jsdom",
+  roots: ['<rootDir>/tests'],
+  testMatch: ['**/*.test.ts', '**/*.test.tsx'],
+  transform: {
+    '^.+\\.tsx?$': [
+      'ts-jest',
+      {
+        tsconfig: './tsconfig.app.json'
+      }
+    ]
+  },
+  moduleNameMapper: {
+    "^@/(.*)$": "<rootDir>/src/$1"
+  },
+  verbose: true,
+  collectCoverage: true,
+  collectCoverageFrom: ['!tests/*', '!**/dist/**/*', '!tests/**/*'],
+  coverageReporters: ['json-summary', 'text'],
+}
---
+++ src/App.tsx
@@ -0,0 +1,5 @@
+// App.tsx
+
+export default function App() {
+    return <div data-testid="root">Home</div>
+}
