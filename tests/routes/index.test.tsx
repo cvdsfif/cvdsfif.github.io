920c9b9a5d25ae2da5957ece91a1f8e0248f851e
Nikit Zykov <nikit@zykov.com>
2024-08-04 21:48:20 +0200

    Link to about page from home

--- tests/routes/index.test.tsx
+++ tests/routes/index.test.tsx
@@ -22,6 +22,7 @@ describe("Testing the home page", () => {
                             })
                         }
                 ),
+                Link: () => <></>
             })
         )
 
